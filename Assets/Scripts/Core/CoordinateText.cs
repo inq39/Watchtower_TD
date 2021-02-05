7f438607c69bfcd48eb57a95e2584b45be1b0f4a
inq39 <61730491+inq39@users.noreply.github.com>
2021-02-05 11:53:47 +0100

    grid, waypoints

---
+++ Assets/Scripts/Core/CoordinateText.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Watchtower.Core
+{
+    [ExecuteAlways]
+    public class CoordinateText : MonoBehaviour
+    {
+        private TextMeshPro _coordinateLabel;
+        private Vector2Int _coordinate = new Vector2Int();
+        private string _coordinateText;
+        // Start is called before the first frame update
+        void Awake()
+        {
+            _coordinateLabel = GetComponentInChildren<TextMeshPro>();
+            UpdateCoordinateText();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!Application.isPlaying)
+            UpdateCoordinateText();
+            UpdateGameObjectText();
+        }
+
+        private void UpdateCoordinateText()
+        {
+            _coordinate.x = Mathf.RoundToInt(transform.position.x / UnityEditor.EditorSnapSettings.move.x);
+            _coordinate.y = Mathf.RoundToInt(transform.position.z / UnityEditor.EditorSnapSettings.move.z);
+            _coordinateText = _coordinate.x + ", " + _coordinate.y;
+            _coordinateLabel.text = _coordinateText;
+        }
+
+        private void UpdateGameObjectText()
+        {
+            transform.name = _coordinateText;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Movement/EnemyMover.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Watchtower.Movement
+{
+    public class EnemyMover : MonoBehaviour
+    {
+        [SerializeField]
+        private List<Waypoint> path = new List<Waypoint>();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
